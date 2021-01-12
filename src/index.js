91a5b603e23081334774d0fa6aecd992076a4ea4
Marco <m.solari@mypass.technology>
2021-01-12 11:58:51 +0100

    debugging heroku error H10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // create an express app
 const express = require("express")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../swagger');
+
+//const { startDatabase } = require('./controllers/db');
+const { dbConnect } = require('./utils/db');
+const usersRoutes = require('./routes/users');
+const itemsRoutes = require('./routes/items');
+const providersRoutes = require('./routes/providers');
+const logger = require('./logger');
+const config = require('./config');
+
 const app = express()
 
 // use the express-static middleware
