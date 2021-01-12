091b2f1cd6b929bd1e6fcd4cc146559f73164607
Marco <m.solari@mypass.technology>
2021-01-12 10:46:57 +0100

    changed port read from env

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ app.use((error, req, res, next) => {
 // connect to the database instance
 dbConnect().then(async () => {
   // start the server
-  app.listen(process.env.PORT /*config.serverPort*/, async () => {
+  app.listen(process.env.PORT || 5000 /*config.serverPort*/, async () => {
     console.log(`listening on port ${config.serverPort}`);
   });
 });
