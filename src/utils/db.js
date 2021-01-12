c602fcc7e3a137677f0210f4634289e97fe762bd
Marco <m.solari@mypass.technology>
2021-01-12 14:40:50 +0100

    debugging heroku error H10

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 exports.dbConnect = async () => {
   try {
     await mongoose
-      .connect(/*config.MONGO_URI*/process.env.MONGO_URI, {
+      .connect(config.MONGO_URI/*process.env.MONGO_URI*/, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true,
